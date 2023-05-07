946ac4621f012766dd43467efe9646f1b2872eee
AdvaitT17 <advait1702@gmail.com>
2023-05-08 05:13:11 +0530

    Update stripe.js

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -9,7 +9,6 @@ export default async function handler(req, res) {
       const params = {
         submit_type: 'pay',
         mode: 'payment',
-        payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
             { shipping_rate: 'shr_1N5GfwSGQvbBlWlJBximX53S' },
