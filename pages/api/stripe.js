6bc7c73463cd05c94270abba3c168c54380c80b3
AdvaitT17 <advait1702@gmail.com>
2023-05-08 17:26:40 +0530

    Update stripe.js
    Signed-off-by: AdvaitT17 <advait1702@gmail.com>

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
 
           return {
             price_data: { 
-              currency: 'inr',
+              currency: 'usd',
               product_data: { 
                 name: item.name,
                 images: [newImage],
